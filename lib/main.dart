ab4bddaba531d6a0c61507317dee466d2cf4c0d0
krupal.bhuva <krupal.bhuva@tatvasoft.com>
2024-03-26 12:02:15 +0530

    url navigation via go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,336 +1,70 @@
-import 'dart:io';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:network_info_plus/network_info_plus.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
-  runApp(MyApp());
+  usePathUrlStrategy();
+  runApp(const MyApp());
 }
 
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeScreen();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'details',
+          builder: (BuildContext context, GoRouterState state) {
+            return const DetailsScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Socket Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+    return MaterialApp.router(
+      routerConfig: _router,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _localIp = '';
-  String _message = '';
-  final TextEditingController _controller = TextEditingController();
-  final TextEditingController ipController = TextEditingController();
-  late ServerSocket _serverSocket;
-
-  @override
-  void initState() {
-    super.initState();
-    _initialize();
-  }
-
-  Future<void> _initialize() async {
-    try {
-      var connectivityResult = await Connectivity().checkConnectivity();
-      if (connectivityResult == ConnectivityResult.wifi) {
-        final info = NetworkInfo();
-        final wifiIp = await info.getWifiIP();
-        print('wifi ip : ${wifiIp}');
-        if (wifiIp != null && wifiIp.isNotEmpty) {
-          // setState(() {
-          //   _localIp = wifiIp;
-          // });
-        } else {
-          setState(() {
-            _message = 'Error: Unable to get local IP address';
-          });
-        }
-      } else {
-        setState(() {
-          _message = 'Error: WiFi is not connected';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-    }
-  }
-
-/*  void _startServer() async {
-    try {
-      _serverSocket = await ServerSocket.bind(_localIp, 4000, shared: true);
-      _serverSocket.listen((Socket socket) {
-        socket.listen(
-              (List<int> data) {
-            setState(() {
-              _message = 'Received from client: ${String.fromCharCodes(data)}';
-            });
-          },
-          onError: (error) {
-            print('Socket listen error: $error');
-          },
-          onDone: () {
-            print('Client disconnected');
-          },
-        );
-      });
-      setState(() {
-        _message = 'Server started';
-      });
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-    }
-  }
-
-  void _sendMessage() async {
-    try {
-      var socket = await Socket.connect(_localIp, 4000);
-      socket.write(_controller.text);
-      socket.close();
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-    }
-  }*/
-
-  /*WebSocket? serverSocket;
-   Future<void> startServer() async {
-    try {
-      var server = await HttpServer.bind(InternetAddress.anyIPv4, 4000);
-      setState(() {
-        _message = 'Server started';
-      });
-
-      server.listen((HttpRequest request) async {
-        if (WebSocketTransformer.isUpgradeRequest(request)) {
-          serverSocket =
-          await WebSocketTransformer.upgrade(request);
-          setState(() {
-            _message = 'Client connected';
-          });
-          serverSocket?.listen((message) {
-            setState(() {
-              _message = 'Received: $message';
-            });
-          }, onError: (error) {
-            setState(() {
-              _message = 'WebSocket error: $error';
-            });
-          }, onDone: () {
-            setState(() {
-              _message = 'Client disconnected';
-            });
-          });
-        }
-      });
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-      print('Error: $e');
-    }
-  }
-
-  WebSocket? clientSocket;
-
-  Future<void> listener() async {
-    try {
-      clientSocket = await WebSocket.connect('ws://localhost:4000');
-      setState(() {
-        _message = 'Connected to server';
-      });
-
-      clientSocket?.listen((message) {
-        setState(() {
-          _message = 'Received: $message';
-        });
-      }, onError: (error) {
-        setState(() {
-          _message = _message = 'WebSocket error: $error';
-        });
-      }, onDone: () {
-        setState(() {
-          _message = 'Server disconnected';
-        });
-      });
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-    }
-  }
-
-  void stopServer() {
-    _serverSocket?.close();
-    setState(() {
-      _message = 'Server stopped';
-    });
-  }
-
-  void sendMessage() async {
-    try {
-      serverSocket !=null ?
-      serverSocket?.add(_controller.text) :
-      clientSocket?.add(_controller.text);
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-    }
-  }*/
-
-  ServerSocket? serverSocket;
-  Future<void> startServer() async {
-    try {
-      // Retrieve the list of network interfaces
-      List<NetworkInterface> interfaces = await NetworkInterface.list();
-
-      // Find the interface corresponding to the Wi-Fi hotspot
-      NetworkInterface wifiHotspotInterface = interfaces.firstWhere(
-        (interface) =>
-            interface.name.contains('wlan') || interface.name.contains('ap'),
-      );
-
-      if (wifiHotspotInterface != null) {
-        // Get the first non-loopback IPv4 address on the hotspot interface
-        InternetAddress hostIpAddress =
-            wifiHotspotInterface.addresses.firstWhere(
-          (address) =>
-              !address.isLoopback && address.type == InternetAddressType.IPv4,
-        );
-
-        var internetIp = InternetAddress.anyIPv4;
-        // Start the server socket
-        var serverSocket = await ServerSocket.bind(internetIp, 4000);
-        setState(() {
-          _message = 'Server started on ${hostIpAddress.address}:4000';
-        });
-        // setState(() {
-        //   _localIp =
-        //       hostIpAddress.address;
-        // });
-        // Listen for incoming connections
-        serverSocket.listen((Socket socket) {
-
-          socket.listen((List<int> data) {
-            String message = String.fromCharCodes(data);
-            setState(() {
-              _message =  'Client connected from ${socket.remoteAddress.address}:${socket.remotePort} , data : $message';
-            });
-
-            socket.write('received: $message');
-          });
-        });
-      } else {
-        setState(() {
-          _message = 'No Wi-Fi hotspot interface found.';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-    }
-  }
-
-  Future<void> listener() async {
-    try {
-      var socket = await Socket.connect(
-          ipController.text.trim(), 4000);
-      _localIp = ipController.text.trim();
-      setState(() {
-        _message = 'stared listening';
-      });
-      socket.listen((data) {
-        print('Received from server: $data');
-        setState(() {
-          _message = 'Received: $data';
-        });
-      });
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-    }
-  }
-
-  void stopServer() {}
-
-  void sendMessage() async {
-    try {
-      var socket = await Socket.connect(_localIp, 4000);
-      socket.write(_controller.text);
-      socket.close();
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-    }
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Socket Demo'),
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go('/details'),
+          child: const Text('Go to the Details screen'),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Local IP: $_localIp'),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: startServer,
-              child: Text('Start Server'),
-            ),
-            TextField(
-              controller: ipController,
-              decoration: InputDecoration(
-                labelText: 'ip add',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: listener,
-              child: Text('Listen Server'),
-            ),
-            SizedBox(height: 8.0),
-            ElevatedButton(
-              onPressed: stopServer,
-              child: Text('Stop Server'),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                labelText: 'Message to send',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: sendMessage,
-              child: Text('Send Message'),
-            ),
-            SizedBox(height: 16.0),
-            Text('$_message'),
-          ],
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Details Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go('/'),
+          child: const Text('Go back to the Home screen'),
         ),
       ),
     );
---
+++ lib/shell_route.dart
@@ -0,0 +1,243 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+
+final GlobalKey<NavigatorState> _rootNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'root');
+final GlobalKey<NavigatorState> _shellNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'shell');
+
+void main() {
+  usePathUrlStrategy();
+  runApp(ShellRouteExampleApp());
+}
+class ShellRouteExampleApp extends StatelessWidget {
+  ShellRouteExampleApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: '/a',
+    debugLogDiagnostics: true,
+    routes: <RouteBase>[
+      ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (BuildContext context, GoRouterState state, Widget child) {
+          return ScaffoldWithNavBar(child: child);
+        },
+        routes: <RouteBase>[
+          GoRoute(
+            path: '/a',
+            builder: (BuildContext context, GoRouterState state) {
+              return const ScreenA();
+            },
+            routes: <RouteBase>[
+              GoRoute(
+                path: 'details',
+                builder: (BuildContext context, GoRouterState state) {
+                  return const DetailsScreen(label: 'A');
+                },
+              ),
+            ],
+          ),
+          GoRoute(
+            path: '/b',
+            builder: (BuildContext context, GoRouterState state) {
+              return const ScreenB();
+            },
+            routes: <RouteBase>[
+              GoRoute(
+                path: 'details',
+                parentNavigatorKey: _rootNavigatorKey,
+                builder: (BuildContext context, GoRouterState state) {
+                  return DetailsScreen(label: state.extra.toString() ?? '');
+                },
+              ),
+            ],
+          ),
+          GoRoute(
+            path: '/c',
+            builder: (BuildContext context, GoRouterState state) {
+              return const ScreenC();
+            },
+            routes: <RouteBase>[
+              GoRoute(
+                path: 'details',
+                builder: (BuildContext context, GoRouterState state) {
+                  return const DetailsScreen(label: 'C');
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routerConfig: _router,
+    );
+  }
+}
+
+class ScaffoldWithNavBar extends StatelessWidget {
+  const ScaffoldWithNavBar({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'A Screen',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'B Screen',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notification_important_rounded),
+            label: 'C Screen',
+          ),
+        ],
+        currentIndex: _calculateSelectedIndex(context),
+        onTap: (int idx) => _onItemTapped(idx, context),
+      ),
+    );
+  }
+
+  static int _calculateSelectedIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.toString();
+    if (location.startsWith('/a')) {
+      return 0;
+    }
+    if (location.startsWith('/b')) {
+      return 1;
+    }
+    if (location.startsWith('/c')) {
+      return 2;
+    }
+    return 0;
+  }
+
+  void _onItemTapped(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        GoRouter.of(context).go('/a');
+      case 1:
+        GoRouter.of(context).go('/b');
+      case 2:
+        GoRouter.of(context).go('/c');
+    }
+  }
+}
+
+class ScreenA extends StatelessWidget {
+  const ScreenA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text('Screen A'),
+            TextButton(
+              onPressed: () {
+                GoRouter.of(context).go('/a/details');
+              },
+              child: const Text('View A details'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ScreenB extends StatelessWidget {
+  const ScreenB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text('Screen B'),
+            TextButton(
+              onPressed: () {
+                GoRouter.of(context).go('/b/details', extra: 'hello world');
+              },
+              child: const Text('View B details'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ScreenC extends StatelessWidget {
+  const ScreenC({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text('Screen C'),
+            TextButton(
+              onPressed: () {
+                GoRouter.of(context).go('/c/details');
+              },
+              child: const Text('View C details'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({
+    required this.label,
+    super.key,
+  });
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Details Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'Details for $label',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+    );
+  }
+}
